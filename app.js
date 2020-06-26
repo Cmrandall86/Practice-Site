52133cd572c0e8988c7e27144d5dc5e06a2e7e46
CHRISTOPHER RANDALL <crandall3@cnm.edu>
2020-06-26 15:02:08 -0600

    Color Changing Header

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('hello')
+
+const button = document.querySelector('#button1');
+
+const header = document.querySelector('#colorChangingHeader');
+
+button.addEventListener("click", handleHeaderButton);
+
+function handleHeaderButton(event) {
+    header.classList.toggle('newHeader');
+
+}
